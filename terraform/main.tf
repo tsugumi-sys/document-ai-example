aa804928b2c9b799b739b9446f60ec8f2c062eeb
tsugumi-sys <tidemark0105@gmail.com>
2024-08-24 15:04:47 +0900

    init

---
+++ terraform/main.tf
@@ -0,0 +1,17 @@
+locals {
+  roles_for_ocr = ["roles/documentai.admin", "roles/storage.admin", "roles/serviceusage.serviceUsageConsumer"]
+}
+
+data "google_project" "project" {}
+
+resource "google_service_account" "default" {
+  account_id = "ocr-example"
+}
+
+resource "google_project_iam_member" "github_actions_iam_workload_identity_user" {
+  for_each = toset(local.roles_for_ocr)
+
+  project = data.google_project.project.id
+  role    = each.value
+  member  = "serviceAccount:${google_service_account.default.email}"
+}
