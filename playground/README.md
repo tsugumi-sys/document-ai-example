b3b565499ebe77edb3e6b4a2932e729ec1dd11e5
tsugumi-sys <tidemark0105@gmail.com>
2024-08-24 16:00:34 +0900

    adding readme

---
+++ playground/README.md
@@ -0,0 +1,2 @@
+Tutorial: https://codelabs.developers.google.com/codelabs/docai-ocr-python
+Handling document result: https://cloud.google.com/document-ai/docs/handle-response#quickstart
--- playground/batch_with_toolbox.py
+++ playground/batch_with_toolbox.py
@@ -1,17 +1,12 @@
-# Now, suppose that you want to read in the text from the entire novel.
-# - Online Processing has limits on the number of pages and file size that can be sent and it only allows for one document file per API call.
-# - Batch Processing allows for processing of larger/multiple files in an asynchronous method.
+"""
+Makes a Batch Processing Request to Document AI using Document AI Toolbox
+"""
 
-"""
-Makes a Batch Processing Request to Document AI
-"""
 import os
-import re
 
 from dotenv import load_dotenv
 from google.api_core.client_options import ClientOptions
-from google.api_core.exceptions import InternalServerError, RetryError
-from google.cloud import documentai, storage
+from google.cloud import documentai, documentai_toolbox
 
 # TODO(developer): Fill these variables before running the sample.
 load_dotenv()
@@ -31,7 +26,7 @@ input_mime_type = "application/pdf"
 field_mask = "text,entities,pages.pageNumber"  # Optional. The fields to return in the Document object.
 
 
-def batch_process_documents(
+def batch_process_toolbox(
     project_id: str,
     location: str,
     processor_id: str,
@@ -40,7 +35,6 @@ def batch_process_documents(
     processor_version_id: str = None,
     input_mime_type: str = None,
     field_mask: str = None,
-    timeout: int = 400,
 ):
     # You must set the api_endpoint if you use a location other than "us".
     opts = ClientOptions(api_endpoint=f"{location}-documentai.googleapis.com")
@@ -88,75 +82,20 @@ def batch_process_documents(
     # BatchProcess returns a Long Running Operation (LRO)
     operation = client.batch_process_documents(request)
 
-    # Continually polls the operation until it is complete.
-    # This could take some time for larger files
-    # Format: projects/{project_id}/locations/{location}/operations/{operation_id}
-    try:
-        print(f"Waiting for operation {operation.operation.name} to complete...")
-        operation.result(timeout=timeout)
-    # Catch exception when operation doesn"t finish before timeout
-    except (RetryError, InternalServerError) as e:
-        print(e.message)
+    # Operation Name Format: projects/{project_id}/locations/{location}/operations/{operation_id}
+    documents = documentai_toolbox.document.Document.from_batch_process_operation(
+        location=location, operation_name=operation.operation.name
+    )
 
-    # NOTE: Can also use callbacks for asynchronous processing
-    #
-    # def my_callback(future):
-    #   result = future.result()
-    #
-    # operation.add_done_callback(my_callback)
-
-    # Once the operation is complete,
-    # get output document information from operation metadata
-    metadata = documentai.BatchProcessMetadata(operation.metadata)
-
-    if metadata.state != documentai.BatchProcessMetadata.State.SUCCEEDED:
-        raise ValueError(f"Batch Process Failed: {metadata.state_message}")
-
-    storage_client = storage.Client()
-
-    print("Output files:")
-    # One process per Input Document
-    for process in list(metadata.individual_process_statuses):
-        # output_gcs_destination format: gs://BUCKET/PREFIX/OPERATION_NUMBER/INPUT_FILE_NUMBER/
-        # The Cloud Storage API requires the bucket name and URI prefix separately
-        matches = re.match(r"gs://(.*?)/(.*)", process.output_gcs_destination)
-        if not matches:
-            print(
-                "Could not parse output GCS destination:",
-                process.output_gcs_destination,
-            )
-            continue
-
-        output_bucket, output_prefix = matches.groups()
-
-        # Get List of Document Objects from the Output Bucket
-        output_blobs = storage_client.list_blobs(output_bucket, prefix=output_prefix)
-
-        # Document AI may output multiple JSON files per source file
-        for blob in output_blobs:
-            # Document AI should only output JSON files to GCS
-            if blob.content_type != "application/json":
-                print(
-                    f"Skipping non-supported file: {blob.name} - Mimetype: {blob.content_type}"
-                )
-                continue
-
-            # Download JSON File as bytes object and convert to Document Object
-            print(f"Fetching {blob.name}")
-            document = documentai.Document.from_json(
-                blob.download_as_bytes(), ignore_unknown_fields=True
-            )
-
-            # For a full list of Document object attributes, please reference this page:
-            # https://cloud.google.com/python/docs/reference/documentai/latest/google.cloud.documentai_v1.types.Document
-
-            # Read the text recognition output from the processor
-            print("The document contains the following text:")
-            print(document.text)
+    for document in documents:
+        # Read the text recognition output from the processor
+        print("The document contains the following text:")
+        # Truncated at 100 characters for brevity
+        print(document.text[:100])
 
 
 if __name__ == "__main__":
-    batch_process_documents(
+    batch_process_toolbox(
         project_id=project_id,
         location=location,
         processor_id=processor_id,
